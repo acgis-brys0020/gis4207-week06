0791d767154d1fff575f26b58f5a2c951a0a9231
Kyle Forsyth <fors0119@algonquinlive.com>
2025-02-11 16:32:23 -0500

    Pushing to help debug

---
+++ SianB_KyleF/data_prep.py
@@ -0,0 +1,49 @@
+
+
+import os
+import sys
+
+
+
+def main():
+
+    global arcpy, env
+    if len(sys.argv) !=4:
+        print("data_prep.py <in_gdbs_base_folder> <out_gdb> <out_feature_dataset>")
+
+
+    import arcpy
+    from arcpy import env
+
+    in_gdbs_base_folder = sys.argv[1]
+
+    out_gdb = sys.argv[2]
+
+    out_feature_datset = sys.argv[3]
+
+    UTM_10_dataprep(in_gdbs_base_folder, out_gdb, out_feature_datset)
+
+
+def UTM_10_dataprep(in_gdbs_base_folder, out_gdb, out_feature_dataset):
+
+    arcpy.env.workspace = in_gdbs_base_folder
+    arcpy.ListWorkspaces(in_gdbs_base_folder)
+    out_folder_path = os.path.dirname(out_gdb)
+
+    Surrey_Working_gdb = arcpy.management.CreateFileGDB(out_folder_path, out_name=out_gdb)
+    
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+    
+
+
+
+
